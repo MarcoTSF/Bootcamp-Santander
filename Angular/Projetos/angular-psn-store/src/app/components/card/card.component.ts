0f908fcdc349ea8c1a5218988e323727b932a654
Marco Túlio Salvador Filho <mtsf.salvador@gmail.com>
2023-10-18 02:27:07 -0300

    feat: project psn store finished

---
+++ Angular/Projetos/angular-psn-store/src/app/components/card/card.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input()
+  gameCover:string = ''
+
+  @Input()
+  gameType:string = ''
+  @Input()
+  gamePrice:string = ''
+
+  @Input()
+  gameLabel:string = ""
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
